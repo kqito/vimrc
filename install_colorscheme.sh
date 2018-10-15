2e3cf60040414fc41294761d93eed4ffb0284b6e
Clq1re <29191111+Clq1re@users.noreply.github.com>
2018-10-16 02:50:53 +0900

    Feat: Make to output when errors

--- install_colorscheme.sh
+++ install_colorscheme.sh
@@ -4,13 +4,23 @@ declare -a site=(\
   "jonathanfilip/vim-lucius/raw/master/colors/lucius.vim" \
   "fcpg/vim-orbital/blob/master/colors/orbital.vim"\
   )
-if type wget > /dev/null 2>&1; then
+color_dir="/usr/local/share/nvim/runtime/colors/"
 
-  color_dir="/usr/local/share/nvim/runtime/colors/"
-  for ((i = 0; i < ${#site[@]}; i++)) {
-    wget https://github.com/${site[i]} -P $color_dir > /dev/null 2>&1
-  }
+function install_colorscheme(){ 
+  if type wget > /dev/null 2>&1; then
+    for ((i = 0; i < ${#site[@]}; i++)) {
+      wget https://github.com/${site[i]} -P $color_dir > /dev/null 2>&1
+    }
+  else
+    cat << EOS
+You have not wget command!!
+Please install it.
+EOS
+exit 1
+fi
+}
 
+function result(){
   printf "Install successfully!!\n\nThe additional colorscheme you can use are\n"
 
   for ((i = 0; i < ${#site[@]}; i++)) {
@@ -20,10 +30,20 @@ if type wget > /dev/null 2>&1; then
 
   printf "\nPlease write it in vimrc as follows.\
     \n\e[32;40mcolorscheme ${filename}"
+}
 
-else
+function error(){
   cat << EOS
-  You have not wget command!!
-  Please install it.
+Someting to wrong!
+Please check to exist the url. 
+Also please check the permissions of colorscheme directory. 
 EOS
-fi
+for ((i = 0; i < ${#site[@]}; i++)) {
+  printf "Colorscheme url $((i+1)): \e[31;4mhttps://github.com/${site[i]}\e[m\n"
+}
+printf "Save location : \e[31;4m$color_dir"
+
+exit
+}
+
+install_colorscheme && result || error
