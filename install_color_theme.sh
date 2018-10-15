48a4235637460d710f5d9bf1bc61d816b3294d22
Clq1re <29191111+Clq1re@users.noreply.github.com>
2018-10-15 20:22:34 +0900

    Feat: Make to download multiple color themes

--- install_color_theme.sh
+++ install_color_theme.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+
+declare -a site=(\
+    "jonathanfilip/vim-lucius/raw/master/colors/lucius.vim" \
+    "fcpg/vim-orbital/blob/master/colors/orbital.vim"\
+    )
 if type wget > /dev/null 2>&1; then
-  wget https://github.com/jonathanfilip/vim-lucius/raw/master/colors/lucius.vim
-  echo 'install successfully!!'
+
+    color_dir="/usr/local/share/nvim/runtime/colors/"
+    for ((i = 0; i < ${#site[@]}; i++)) {
+        wget https://github.com/${site[i]} -P $color_dir > /dev/null 2>&1
+    }
+
+    echo "Install successfully!!\nThe additional colortheme you can use are\n"
+
+    for ((i = 0; i < ${#site[@]}; i++)) {
+        echo $((i+1)). ${site[i]##*/} | sed -e 's/\.vim//'
+    }
+
 else
-  cat << EOS
-  you have not wget command!!
-  please install it.
+    cat << EOS
+  You have not wget command!!
+  Please install it.
 EOS
 fi
