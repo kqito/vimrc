6ced1074c6cb5462288607815b278d08fd3f52ec
Clq1re <29191111+Clq1re@users.noreply.github.com>
2018-10-16 00:55:11 +0900

    Feat: Make to output the result with coloring

--- install_colorscheme.sh
+++ install_colorscheme.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 declare -a site=(\
-    "jonathanfilip/vim-lucius/raw/master/colors/lucius.vim" \
-    "fcpg/vim-orbital/blob/master/colors/orbital.vim"\
-    )
+  "jonathanfilip/vim-lucius/raw/master/colors/lucius.vim" \
+  "fcpg/vim-orbital/blob/master/colors/orbital.vim"\
+  )
 if type wget > /dev/null 2>&1; then
 
-    color_dir="/usr/local/share/nvim/runtime/colors/"
-    for ((i = 0; i < ${#site[@]}; i++)) {
-        wget https://github.com/${site[i]} -P $color_dir > /dev/null 2>&1
-    }
+  color_dir="/usr/local/share/nvim/runtime/colors/"
+  for ((i = 0; i < ${#site[@]}; i++)) {
+    wget https://github.com/${site[i]} -P $color_dir > /dev/null 2>&1
+  }
 
-    echo "Install successfully!!\nThe additional colorscheme you can use are"
+  printf "Install successfully!!\n\nThe additional colorscheme you can use are\n"
 
-    for ((i = 0; i < ${#site[@]}; i++)) {
-        echo $((i+1)). ${site[i]##*/} | sed -e 's/\.vim//'
-    }
+  for ((i = 0; i < ${#site[@]}; i++)) {
+    filename=${site[i]##*/}
+    printf "\e[31;4m $((i+1)). ${filename%.*}\e[m\n"
+  }
+
+  printf "\nPlease write it in vimrc as follows.\
+    \n\e[32;40mcolorscheme ${filename}"
 
 else
-    cat << EOS
+  cat << EOS
   You have not wget command!!
   Please install it.
 EOS
