235735e094f5df6150a5e5541a8cd54f7fd9626b
kqito <29191111+kqito@users.noreply.github.com>
2020-01-10 16:37:38 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 
-scriptDirectory=($(cd $(dirname $BASH_SOURCE); pwd))
+declare -a VIMRC_DIRS=("${HOME}/.vim" "${HOME}/.config/nvim")
 
-# Create nvim dir
-mkdir -p ~/.config/
+check() {
+  read -p "$1 (y/N): " yn
 
-# Copy vimrc files
-`cp -r "${scriptDirectory}" ~/.config/nvim`
-`cp -r "${scriptDirectory}" ~/.vim`
+  case "$yn" in
+    [yY]*) return 0 ;;
+    *) return 1 ;;
+  esac
+}
+
+install() {
+  SCRIPT_DIR=($(cd $(dirname $BASH_SOURCE); pwd))
+
+  echo "Installing kqito's vimrc..."
+
+  mkdir -p ~/.config/
+
+  for dir in ${VIMRC_DIRS[@]}; do
+    rm -rf ${dir}
+    cp -r ${SCRIPT_DIR} ${dir}
+  done
+
+  echo "Done!!"
+}
+
+isEmptyVimrcDir() {
+  isEmpty=0
+
+  for dir in ${VIMRC_DIRS[@]}; do
+    if [ ! -z "$(ls -A ${dir})" ]; then
+      echo "Your vimrc is exist!! => ${dir}"
+      isEmpty=1
+    fi
+  done
+
+  [ ${isEmpty} -eq 0 ] && return 0 || return 1
+}
+
+
+! isEmptyVimrcDir && $(! check "Remove all your vimrc to install kqito's vimrc?") && exit 0
+
+install
