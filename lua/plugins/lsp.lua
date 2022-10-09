1a8a78085f7a92ffdd90f2d35e2bee9988c89b54
kqito <29191111+kqito@users.noreply.github.com>
2022-10-09 15:31:03 +0900

    feat: add formatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,148 +1,196 @@
 -- LSP Mappings
-vim.keymap.set('n', '<space>d',  '<cmd>lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n', '<space>.', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-vim.keymap.set('n', '<space>ra', '<cmd>lua vim.lsp.buf.rename()<CR>')
-vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
-vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
--- vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
--- vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set("n", "<space>d", "<cmd>lua vim.lsp.buf.hover()<CR>")
+vim.keymap.set("n", "<space>.", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+vim.keymap.set("n", "<space>ra", "<cmd>lua vim.lsp.buf.rename()<CR>")
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+vim.keymap.set("n", "ge", "<cmd>lua vim.diagnostic.open_float()<CR>")
+vim.keymap.set("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+vim.keymap.set("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+vim.keymap.set("n", "<space>e", "<cmd>TroubleToggle<cr>")
 
--- LSP handlers
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = {
-    prefix = '●'
-  }
-})
-vim.o.updatetime = 250
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-
-local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
 -- Leading keys to use LSP function
-local leader = '<LocalLeader>'
+local leader = "<LocalLeader>"
 
 -- Custom border of hover window
 _G.__MyLspFloatingOpts = {
-  focusable = false,
-  border = {
-    {'+', 'FloatBorder'},
-    {'-', 'FloatBorder'},
-    {'+', 'FloatBorder'},
-    {'|', 'FloatBorder'},
-    {'+', 'FloatBorder'},
-    {'-', 'FloatBorder'},
-    {'+', 'FloatBorder'},
-    {'|', 'FloatBorder'},
-  },
+	focusable = false,
+	border = {
+		{ "+", "FloatBorder" },
+		{ "-", "FloatBorder" },
+		{ "+", "FloatBorder" },
+		{ "|", "FloatBorder" },
+		{ "+", "FloatBorder" },
+		{ "-", "FloatBorder" },
+		{ "+", "FloatBorder" },
+		{ "|", "FloatBorder" },
+	},
 }
 
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 local on_attach = function(client, bufnr)
-  local function lspmap(m) return leader .. m end
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-  local opts = { noremap=true, silent=true }
+	local function lspmap(m)
+		return leader .. m
+	end
 
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, _G.__MyLspFloatingOpts)
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-  -- Highlight a symbol and its references when holding the cursor
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
+
+	local opts = { noremap = true, silent = true }
+
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, _G.__MyLspFloatingOpts)
+	vim.lsp.handlers["textDocument/publishDiagnostics"] =
+		vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = {
+			prefix = "●",
+		} })
+
+	local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
+
+	-- Highlight a symbol and its references when holding the cursor
+	if client.server_capabilities.document_highlight then
+		vim.api.nvim_exec(
+			[[
       augroup MyAutoCmdLspDocumentHighlight
         autocmd! * <buffer>
         autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]], false)
-  end
+    ]],
+			false
+		)
+	end
 
-  -- Show diagnostics which current line includes
-  vim.api.nvim_exec([[
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
+
+	-- Show diagnostics which current line includes
+	vim.api.nvim_exec(
+		[[
     augroup MyAutoCmdLspDiagnostics
       autocmd!
       autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(0, _G.__MyLspFloatingOpts)
     augroup END
-  ]], false)
-
-  -- Add `:Format` command
-  if client.resolved_capabilities.document_formatting then
-    buf_set_keymap('n', lspmap('f'), '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  elseif client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap('n', lspmap('f'), '<cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
-  end
+  ]],
+		false
+	)
 end
 
 local lsp_settings = {
-  bashls = {},
-  denols = {
-    root_dir = lspconfig.util.root_pattern("deno.json"),
-    init_options = { lint = true },
-  },
-  dockerls = {},
-  gopls = {},
-  html = {},
-  jsonls = {},
-  solargraph = {},
-  sumneko_lua = {},
-  terraformls = {},
-  tflint = {},
-  tsserver = {
-    root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json"),
-  },
-  vimls = {},
-  yamlls = {
-    settings = {
-      yaml = {
-        schemas = {
-          kubernetes = "/*.yaml",
-          ["https://json.schemastore.org/ansible-playbook.json"] = { "/playbook.yml", "/ansible/*.yml" },
-          ["https://json.schemastore.org/ansible-role-2.9.json"] = { "/roles/tasks/*.yml" },
-        },
-      },
-    },
-  },
+	bashls = {},
+	denols = {
+		root_dir = lspconfig.util.root_pattern("deno.json"),
+		init_options = { lint = true },
+	},
+	dockerls = {},
+	gopls = {},
+	html = {},
+	jsonls = {},
+	solargraph = {},
+	sumneko_lua = {},
+	terraformls = {},
+	tflint = {},
+	tsserver = {
+		root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json"),
+	},
+	vimls = {},
+	yamlls = {
+		settings = {
+			yaml = {
+				schemas = {
+					kubernetes = "/*.yaml",
+					["https://json.schemastore.org/ansible-playbook.json"] = { "/playbook.yml", "/ansible/*.yml" },
+					["https://json.schemastore.org/ansible-role-2.9.json"] = { "/roles/tasks/*.yml" },
+				},
+			},
+		},
+	},
 }
 
 local required_server_names = {}
 for server_name, _ in pairs(lsp_settings) do
-  table.insert(required_server_names, server_name)
+	table.insert(required_server_names, server_name)
 end
 
-local capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require('mason').setup {
-}
+require("mason").setup({})
 
-require('mason-lspconfig').setup {
-  ensure_installed = required_server_names
-}
+require("mason-lspconfig").setup({
+	ensure_installed = required_server_names,
+})
 
-require('mason-lspconfig').setup_handlers {
-  function(server_name)
-    local opts = {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    }
-    -- Override settings
-    if lsp_settings[server_name] ~= nil then
-      for k, v in pairs(lsp_settings[server_name]) do
-        opts[k] = v
-      end
-    end
-    lspconfig[server_name].setup(opts)
-    vim.cmd [[ do User LspAttachBuffers ]]
-  end,
-}
+require("mason-lspconfig").setup_handlers({
+	function(server_name)
+		local opts = {
+			on_attach = on_attach,
+			capabilities = capabilities,
+		}
+		-- Override settings
+		if lsp_settings[server_name] ~= nil then
+			for k, v in pairs(lsp_settings[server_name]) do
+				opts[k] = v
+			end
+		end
+		lspconfig[server_name].setup(opts)
+		vim.cmd([[ do User LspAttachBuffers ]])
+	end,
+})
 
-require("trouble").setup {}
-vim.keymap.set("n", "<space>e", "<cmd>TroubleToggle<cr>")
+require("trouble").setup({})
+
+local null_ls = require("null-ls")
+null_ls.setup({
+	-- @see: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+	sources = {
+		-- JavaScript
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.code_actions.eslint,
+		null_ls.builtins.formatting.prettier,
+
+		-- Rust
+		null_ls.builtins.formatting.rustfmt,
+		null_ls.builtins.code_actions.ltrs,
+
+		-- Go
+		null_ls.builtins.formatting.gofmt,
+
+		-- Misc
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.diagnostics.luacheck,
+		null_ls.builtins.diagnostics.yamllint,
+	},
+})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,56 +1,58 @@
-local builtin = require('telescope.builtin')
-local actions = require('telescope.actions')
+local builtin = require("telescope.builtin")
+local actions = require("telescope.actions")
 
 local function telescope_builtin_oldfiles()
-  builtin.oldfiles({
-      only_cwd = true
-    -- {cwd_only} (boolean)  alias for only_cwd
-  })
+	builtin.oldfiles({
+		only_cwd = true,
+		-- {cwd_only} (boolean)  alias for only_cwd
+	})
 end
 
-vim.keymap.set('n', 'sp', builtin.find_files)
-vim.keymap.set('n', 'ss', builtin.grep_string)
-vim.keymap.set('n', 'sg', builtin.live_grep)
-vim.keymap.set('n', 'sb', builtin.buffers)
-vim.keymap.set('n', 'sh', builtin.help_tags)
-vim.keymap.set('n', 'sw', builtin.lsp_workspace_symbols)
-vim.keymap.set('n', 'so', builtin.lsp_document_symbols)
-vim.keymap.set('n', 'se', builtin.diagnostics)
-vim.keymap.set('n', 'sc', builtin.commands)
-vim.keymap.set('n', 'sy', telescope_builtin_oldfiles)
+vim.keymap.set("n", "sp", builtin.find_files)
+vim.keymap.set("n", "ss", builtin.grep_string)
+vim.keymap.set("n", "sg", builtin.live_grep)
+vim.keymap.set("n", "sb", builtin.buffers)
+vim.keymap.set("n", "sh", builtin.help_tags)
+vim.keymap.set("n", "sw", builtin.lsp_workspace_symbols)
+vim.keymap.set("n", "so", builtin.lsp_document_symbols)
+vim.keymap.set("n", "se", builtin.diagnostics)
+vim.keymap.set("n", "sc", builtin.commands)
+vim.keymap.set("n", "sy", telescope_builtin_oldfiles)
 
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-t>'] = false, -- For tmux binding
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-        ["<C-e>"] = actions.file_tab,
-        ["<esc>"] = actions.close,
-        ["<CR>"] = actions.select_default + actions.center
-      },
-      n = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist
-      }
-    }
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-    }
-  },
-  color_devicons = true,
-}
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-u>"] = false,
+				["<C-t>"] = false, -- For tmux binding
+				["<C-h>"] = false, -- For tmux binding
+				["<C-l>"] = false, -- For tmux binding
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+				["<C-e>"] = actions.file_tab,
+				["<esc>"] = actions.close,
+				["<CR>"] = actions.select_default + actions.center,
+			},
+			n = {
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+			},
+		},
+	},
+	extensions = {
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		},
+	},
+	color_devicons = true,
+})
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
+require("telescope").load_extension("fzf")
