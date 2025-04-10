331311f1b1b13a17d0456c6ba1762fe3e750cd61
kqito <29191111+kqito@users.noreply.github.com>
2025-04-10 16:06:16 +0900

    update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -31,76 +31,122 @@ return {
     end,
   },
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    opts = {
-      -- add any opts here
-      -- for example
-      provider = "copilot",
-      auto_suggestions_provider = "copilot",
-      behaviour = {
-        auto_suggestions = true,
-        auto_set_highlight_group = true,
-        auto_set_keymaps = true,
-        auto_apply_diff_after_generation = true,
-        support_paste_from_clipboard = true,
-      },
-      -- windows = {
-      --   position = "right",
-      --   width = 30,
-      --   sidebar_header = {
-      --     align = "center",
-      --     rounded = false,
-      --   },
-      --   ask = {
-      --     floating = true,
-      --     start_insert = true,
-      --     border = "rounded"
-      --   }
-      -- },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-      "github/copilot.vim",            -- for providers='copilot'
-      -- "zbirenbaum/copilot.lua",        -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    keys = {
+      { "<space>aa", "<cmd>CopilotChatToggle<cr>", desc = "Avante Chat" },
+    },
+    config = function()
+      require("CopilotChat").setup({
+        mappings = {
+          -- Resolve configuration conflicts Tab/S-Tab mappings
+          complete = {
+            detail = "Use @<Tab> or /<Tab> for options.",
+            insert = "",
+          },
+          submit_prompt = {
+            normal = '<CR>',
+            -- insert = '<C-CR>',
           },
         },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  }
+        model = 'gpt-4o-mini',
+        agent = 'copilot',
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "copilot-chat",
+        callback = function()
+          vim.api.nvim_buf_set_keymap(0, "i", "<Tab>", [[pumvisible() ? '<C-n>' : '<Tab>']],
+            { expr = true, noremap = true })
+          vim.api.nvim_buf_set_keymap(0, "i", "<S-Tab>", [[pumvisible() ? '<C-p>' : '<S-Tab>']],
+            { expr = true, noremap = true })
+        end,
+      })
+      vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = 'copilot-*',
+        callback = function()
+          -- Set buffer-local options
+          vim.opt_local.relativenumber = false
+          vim.opt_local.number = false
+          vim.opt_local.conceallevel = 0
+        end
+      })
+    end,
+  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  --   opts = {
+  --     -- add any opts here
+  --     -- for example
+  --     provider = "copilot",
+  --     auto_suggestions_provider = "copilot",
+  --     behaviour = {
+  --       auto_suggestions = true,
+  --       auto_set_highlight_group = true,
+  --       auto_set_keymaps = true,
+  --       auto_apply_diff_after_generation = true,
+  --       support_paste_from_clipboard = true,
+  --     },
+  --     -- windows = {
+  --     --   position = "right",
+  --     --   width = 30,
+  --     --   sidebar_header = {
+  --     --     align = "center",
+  --     --     rounded = false,
+  --     --   },
+  --     --   ask = {
+  --     --     floating = true,
+  --     --     start_insert = true,
+  --     --     border = "rounded"
+  --     --   }
+  --     -- },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "echasnovski/mini.pick",         -- for file_selector provider mini.pick
+  --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+  --     "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+  --     "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+  --     "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+  --     "github/copilot.vim",            -- for providers='copilot'
+  --     -- "zbirenbaum/copilot.lua",        -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- }
 }
